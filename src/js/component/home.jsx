a3ad3e97fdbc4a529fa84be65f60ee896b2ca339
EdgarDMen <123202799+EdgarDMen@users.noreply.github.com>
2023-04-18 01:04:14 +0000

    complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const TrafficLight = () => {
+  const [color, setColor] = useState('red');
+  const [selectedLight, setSelectedLight] = useState('');
+
+  useEffect(() => {
+    const changeColor = () => {
+      setColor((prevColor) => {
+        if (prevColor === 'red') return 'yellow';
+        if (prevColor === 'yellow') return 'green';
+        return 'red';
+      });
+    };
+
+    const intervalId = setInterval(changeColor, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleClick = (light) => {
+    setSelectedLight(light);
+    setColor(light);
+  };
+
+  const lightStyle = (lightColor) => ({
+    backgroundColor: color === lightColor ? lightColor : 'gray',
+    boxShadow: selectedLight === lightColor ? '0 0 10px 5px rgba(0, 0, 0, 0.4)' : 'none',
+  });
+
+  return (
+    <div className="traffic-light">
+      <div
+        className="light red"
+        style={lightStyle('red')}
+        onClick={() => handleClick('red')}
+      ></div>
+      <div
+        className="light yellow"
+        style={lightStyle('yellow')}
+        onClick={() => handleClick('yellow')}
+      ></div>
+      <div
+        className="light green"
+        style={lightStyle('green')}
+        onClick={() => handleClick('green')}
+      ></div>
+    </div>
+  );
 };
 
+ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
 export default Home;
